e3e604cbe7a40dceffc7d3281cc0b0ca08638e34
Enrique, MEngEE DSP <141517235+perazaharmonics@users.noreply.github.com>
2024-02-22 18:40:10 +0000

    Symmetric FIR filter

---
+++ symFIR.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sp
+import scipy.signal as sig
+
+"""
+Symmetric FIR Filter: Consider the impulse response 
+h(n) = [1, 1]
+a) Determine the frequency response H(e^jw) of the filter.
+b) Determine the phase delay of the filter.
+c) Determine the group delay of the filter.
+"""
+
+# a) Determine the frequency response H(e^jw) of the filter.
+h = np.array([1, 1])
+w, H = sig.freqz(h)
+
+# b) Determine the phase delay of the filter.
+phase = np.unwrap(np.angle(H))
+phase_delay = -phase / w
+# Fix division by zero for phase delay at w=0
+phase_delay[0] = phase_delay[1]
+
+# c) Determine the group delay of the filter.
+w_gd, group_delay = sig.group_delay((h, 1))
+group_delay = np.array(group_delay)
+
+
+# Plotting Frequency Response
+
+plt.subplot(3, 1, 1)
+plt.plot(w, abs(H))
+plt.title('Frequency Response')
+plt.xlabel('Frequency (rad/sample)')
+plt.ylabel('Magnitude')
+
+# Plotting Phase Delay
+plt.subplot(3, 1, 2)
+plt.plot(w, phase_delay)
+plt.title('Phase Delay')
+plt.xlabel('Frequency (rad/sample)')
+plt.ylabel('Phase Delay (samples)')
+
+# Plotting Group Delay
+plt.subplot(3, 1, 3)
+plt.plot(w_gd, group_delay)
+plt.title('Group Delay')
+plt.xlabel('Frequency (rad/sample)')
+plt.ylabel('Group Delay (samples)')
+
+
+plt.tight_layout()
+plt.show()
